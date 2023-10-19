7076b12849b2dd737bf0b072af8ee9e38d6d351f
natalija <gjorgjieva93@gmail.com>
2023-10-19 12:22:07 -0400

    uploaded String_SameLetter class into week4 package

--- src/natalija/week4/String_SameLetter.java
+++ src/natalija/week4/String_SameLetter.java
@@ -1,5 +1,7 @@
 package natalija.week4;
 
+import java.util.Arrays;
+
 public class String_SameLetter {
 
     //String -- Same letters
@@ -7,5 +9,25 @@ public class String_SameLetter {
     //same letters as another string.
     //Ex: same("abc", "cab"); -> true
     //same("abc", "abb"); -> false:
+
+    public static void main(String[] args) {
+
+        String str1 = "abc";
+        String str2 = "cab";
+
+        System.out.println("sameChars(str1, str2) = " + sameChars(str1, str2));
+
+    }
+
+    public static boolean sameChars(String str1, String str2){
+
+        char[] first = str1.toCharArray();
+        char[] second = str2.toCharArray();
+        Arrays.sort(first);
+        Arrays.sort(second);
+
+        return Arrays.equals(first, second);
+
+    }
 
 }
