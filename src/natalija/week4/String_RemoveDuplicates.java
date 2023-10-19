78519d35ebd42b11922ad709590d38d76249e16f
natalija <gjorgjieva93@gmail.com>
2023-10-19 12:31:11 -0400

    uploaded String_RemoveDu  class into week4 package

--- src/natalija/week4/String_RemoveDuplicates.java
+++ src/natalija/week4/String_RemoveDuplicates.java
@@ -3,8 +3,27 @@ package natalija.week4;
 public class String_RemoveDuplicates {
 
     //String -- Remove Duplicates
-    //Write a return method that can remove the duplicated values from
-    //String
+    //Write a return method that can remove the duplicated values from String
     //Ex: removeDup("AAABBBCCC") ==> ABC
+
+    public static void main(String[] args) {
+
+        String str = "AAABBBCCC";
+        System.out.println("removeDuplicates(str) = " + removeDuplicates(str));
+
+    }
+
+
+    public static String removeDuplicates(String str) {
+
+        String result = "";
+
+        for(int i = 0; i < str.length(); i++){
+            if(!result.contains(String.valueOf(str.charAt(i)))){
+                result += String.valueOf(str.charAt(i));
+            }
+        }
+        return result;
+    }
 
 }
